7c5b848559248728f7d12eb2770d40eee45b097e
PiotrFLEURY <piotr.fleury@gmail.com>
2025-04-19 10:27:38 +0200

    fix(#26): lines with export-statement not ignored

--- flutter_example/lib/not_tested.dart
+++ flutter_example/lib/not_tested.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+export 'package:flutter/foundation.dart'; // Should be ignored
+
 class NotTested {
   void notTested() {
     debugPrint('This function is not tested');
